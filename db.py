bcc6fb5bdda50203143121e0db46ea16afc70d45
rossotto <rossaotto@gmail.com>
2018-03-14 13:52:51 -0400

    Started adding Post form and Ross's database

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from flask import g
 import psycopg2
 import psycopg2.extras
 
-data_source_name = 'host=faraday.cse.taylor.edu dbname=joeyferg user=joeyferg password=kavibeda'
+data_source_name = 'host=faraday.cse.taylor.edu dbname=rrc4 user=rrc4 password=decisage'
 
 
 def open_db_connection():
