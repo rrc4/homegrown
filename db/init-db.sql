beec516828f01a13ca81251993d2f74c63270eeb
jferguso <joseph_ferguson@taylor.edu>
2018-04-14 01:38:37 -0400

    Tried to update the initial data to have pictures but was unsuccessful

--- db/init-db.sql
+++ db/init-db.sql
@@ -1,6 +1,6 @@
 INSERT INTO "user" (name, email, password, rating, active) VALUES ('Joey Ferguson', 'joey@example.com', 'password', 5.0, TRUE);
 INSERT INTO "user" (name, email, password, rating, active) VALUES ('Jake Smarrella', 'jake@example.com', 'password', 4.0, TRUE);
-INSERT INTO "user" (name, email, password, rating, active) VALUES ('Joe Schuette', 'joe@example.com', 'password', 3.0, FALSE);
+INSERT INTO "user" (name, email, password, rating, active) VALUES ('Joe Schuette', 'joe@example.com', 'password', 3.0, TRUE);
 INSERT INTO "user" (name, email, password, rating, active) VALUES ('Ross Otto', 'ross@example.com', 'password', 2.0, TRUE);
 INSERT INTO "user" (name, email, password, rating, active) VALUES ('Harry VanDerNoord', 'harry@example.com', 'password', 1.0, TRUE);
 
@@ -9,6 +9,12 @@ INSERT INTO post (user_id, price, quantity, product, category, loc, description)
 INSERT INTO post (user_id, price, quantity, product, category, loc, description) VALUES (3, 99.00, 50, 'Beef', 'Meat', 'Gas City', 'Sample Description');
 INSERT INTO post (user_id, price, quantity, product, category, loc, description) VALUES (3, 99.00, 50, 'Milk', 'Dairy', 'Gas City', 'Sample Description');
 INSERT INTO post (user_id, price, quantity, product, category, loc, description) VALUES (4, 1.25, 1, 'Bread', 'Grains', 'Marion', 'Sample Description');
+
+-- UPDATE photo SET file_path = 'file0001.jpg' WHERE id = 1;
+-- UPDATE photo SET file_path = 'file0002.jpg' WHERE id = 2;
+-- UPDATE photo SET file_path = 'file0003.jpg' WHERE id = 3;
+-- UPDATE photo SET file_path = 'file0004.jpg' WHERE id = 4;
+-- UPDATE photo SET file_path = 'file0005.jpg' WHERE id = 5;
 
 INSERT INTO favorite (user_id, post_id) VALUES (1, 1);
 INSERT INTO favorite (user_id, post_id) VALUES (1, 2);
