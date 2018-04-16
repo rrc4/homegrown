0a3a92cb2af572a5bebedd73d6ef33ac749a6b74
Harrison VanDerNoord <harrison_vandernoord@taylor.edu>
2018-04-16 13:43:53 -0400

    Switched to config file

--- db.py
+++ db.py
@@ -6,9 +6,6 @@ import config
 
 data_source_name = config.data_source_name
 
-# data_source_name = 'host=faraday.cse.taylor.edu dbname=rrc4 user=rrc4 password=decisage'
-# data_source_name = 'host=faraday.cse.taylor.edu dbname=harrisonvdn user=harrisonvdn password=mudojose'
-
 
 # Open database connection
 def open_db_connection():
