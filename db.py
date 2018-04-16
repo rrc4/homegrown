77976e57657788e3149a3d018f5567537b67389b
joeschuette <joe_schuette@taylor.edu>
2018-04-16 13:13:33 -0400

    Switch to config file for database connection

--- db.py
+++ db.py
@@ -4,10 +4,8 @@ import psycopg2.extras
 
 import config
 
-# data_source_name = config.data_source_name
+data_source_name = config.data_source_name
 
-''' Uncomment your database before working on your code, and comment it out again when pushing '''
-# data_source_name = 'host=faraday.cse.taylor.edu dbname=joeschuette user=joeschuette password=kahilewo'
 # data_source_name = 'host=faraday.cse.taylor.edu dbname=rrc4 user=rrc4 password=decisage'
 # data_source_name = 'host=faraday.cse.taylor.edu dbname=esmarrel user=esmarrel password=mowozate'
 # data_source_name = 'host=faraday.cse.taylor.edu dbname=harrisonvdn user=harrisonvdn password=mudojose'
