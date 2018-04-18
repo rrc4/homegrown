ffef3f13b3c830b46198e09ec9e63960f3ec70de
rossotto <rossaotto@gmail.com>
2018-04-18 14:02:34 -0400

    Not flashing unnecessary message

--- application.py
+++ application.py
@@ -506,7 +506,6 @@ def all_posts():
             if not key_list:
                 return render_template('posts.html', date=date, filter_form=selected, search_form=query, posts=db.all_posts(), mode='results')
             else:
-                flash('No Results Found', category='danger')
                 return render_template('posts.html', date=date, filter_form=selected, search_form=query, posts=[], mode='results')
         else:
             return render_template('posts.html', date=date, filter_form=selected, search_form=query, posts=filtered_posts, mode='results')
